66737d1f8475e7cc2f0168be78ebf89113de6cb5
HaoJun <rin841005@gmail.com>
2017-05-08 01:00:01 +0800

    add GroupMember, update GroupInfo

--- app/src/main/java/ya/haojun/roadtoadventure/activity/GroupInfoActivity.java
+++ app/src/main/java/ya/haojun/roadtoadventure/activity/GroupInfoActivity.java
@@ -2,25 +2,49 @@ package ya.haojun.roadtoadventure.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import ya.haojun.roadtoadventure.R;
 
-public class GroupInfoActivity extends AppCompatActivity {
+public class GroupInfoActivity extends CommonActivity implements View.OnClickListener {
 
     // ui
     private ImageView iv_group_picture;
     private TextView tv_group_name;
     // extra
     private int groupID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_info);
 
         // ui reference
+        iv_group_picture = (ImageView) findViewById(R.id.iv_group_info_picture);
+        tv_group_name = (TextView) findViewById(R.id.tv_group_info_name);
+        findViewById(R.id.cv_group_info_member).setOnClickListener(this);
+        findViewById(R.id.cv_group_info_chat).setOnClickListener(this);
         // extra
         groupID = getIntent().getExtras().getInt("groupID");
+        // init
+        int w = getResources().getDisplayMetrics().widthPixels;
+        int h = (int) (getResources().getDisplayMetrics().density * 192);
+        Picasso.with(this).load("http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/muchdoge-700x393.jpg").resize(w, h).centerCrop().into(iv_group_picture);
+        tv_group_name.setText("熱血騎士");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cv_group_info_member:
+                openActivity(GroupMemberActivity.class);
+                break;
+            case R.id.cv_group_info_chat:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/ya/haojun/roadtoadventure/activity/GroupMemberActivity.java
@@ -0,0 +1,36 @@
+package ya.haojun.roadtoadventure.activity;
+
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import ya.haojun.roadtoadventure.R;
+import ya.haojun.roadtoadventure.adapter.GroupMemberRVAdapter;
+import ya.haojun.roadtoadventure.model.GroupMember;
+
+public class GroupMemberActivity extends CommonActivity {
+
+    // ui
+    private RecyclerView rv_member;
+    // data
+    private ArrayList<GroupMember> list_member;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_member);
+
+        // ui reference
+        rv_member = (RecyclerView) findViewById(R.id.rv_group_member);
+
+        // init RecyclerView
+        list_member = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list_member.add(new GroupMember("haojun" + i, "https://avatars1.githubusercontent.com/u/15250400?v=3&s=460"));
+        }
+        rv_member.setLayoutManager(new GridLayoutManager(this, 3));
+        rv_member.setAdapter(new GroupMemberRVAdapter(this, list_member));
+    }
+}
